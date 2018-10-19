cb3921a0756c11e367ad4e54cb2c285ebdd5ed01
xiaozhx <xiaozx@shinetechsoftware.com>
2018-10-19 17:58:06 +0800

    test config

--- AutomatedTestingProject/config/configuration.py
+++ AutomatedTestingProject/config/configuration.py
@@ -1,6 +1,32 @@
 # coding=utf-8
 
 import os
+import logging
+from configparser import ConfigParser
+import schedule
+import time
+
+
+class TestClass():
+    def test_fun(self):
+        logger = logging.getLogger(self.__class__.__name__)
+        fmt = '%(asctime)s - %(filename)s - %(name)s - %(funcName)s - %(lineno)d - %(levelname)s - %(message)s'
+        logger.setLevel(logging.DEBUG)
+        sh = logging.StreamHandler()
+        sh.setFormatter(logging.Formatter(fmt))
+        logger.addHandler(sh)
+        logger.debug('hello world')
+
+
+def job():
+    print('working')
+
 
 if __name__ == '__main__':
-    print(os.path.dirname(__file__)+os.path.sep)
+    tc = TestClass()
+    schedule.every(5).seconds.do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
--- AutomatedTestingProject/config/enve_configure.py
+++ AutomatedTestingProject/config/enve_configure.py
@@ -7,6 +7,23 @@ from project import base_project_path
 req_path = "requirements.txt"
 # ChromeDriver淘宝下载镜像链接
 chrome_driver_link = "http://npm.taobao.org/mirrors/chromedriver/2.43/chromedriver_win32.zip"
+firefox_driver_link = "https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-win64.zip"
+
+
+def download_and_unzip_drivers(driver_link):
+    right_index = driver_link.rfind('/')
+    zip_file_name = driver_link[right_index+1:len(driver_link)]
+    import requests as req
+    response = req.get(driver_link)
+    with open(base_project_path + r"assets/driver_files/"+zip_file_name, "wb") as driver_file:
+        driver_file.write(response.content)
+    print(zip_file_name + " has been downloaded")
+    zip_file = zipfile.ZipFile(base_project_path + r"assets/driver_files/"+zip_file_name, "r")
+    for file in zip_file.namelist():
+        zip_file.extract(file, base_project_path + r'assets/driver_files/')
+    zip_file.close()
+    print(zip_file_name + " has been extract into assets/driver_files/")
+
 
 if __name__ == '__main__':
     # 自动安装环境依赖
@@ -16,19 +33,11 @@ if __name__ == '__main__':
     result_str = result_cmd.read()
     print(result_str)
     # 自动获取Chrome Driver文件
-    if not os.path.exists(base_project_path + r'assets/drivers'):
-        os.mkdir(base_project_path + r'assets/drivers')
+    if not os.path.exists(base_project_path + r'assets/driver_files'):
+        os.mkdir(base_project_path + r'assets/driver_files')
     try:
-        import requests as req
-        response = req.get(chrome_driver_link)
-        with open(base_project_path + r"assets/drivers/chromedriver_win32.zip", "wb") as driver_file:
-            driver_file.write(response.content)
-        print("chromedriver_win32.zip has been downloaded")
-        zip_file = zipfile.ZipFile(base_project_path + r"assets/drivers/chromedriver_win32.zip", "r")
-        for file in zip_file.namelist():
-            zip_file.extract(file, base_project_path + r'assets/drivers/')
-        zip_file.close()
-        print("chromedriver_win32.zip has been extract into assets/drivers/")
+        download_and_unzip_drivers(chrome_driver_link)
+        download_and_unzip_drivers(firefox_driver_link)
     except Exception as error:
         print(error)
-        print("error with getting chromedriver_win32.zip")
+        print("error with getting driver files")
